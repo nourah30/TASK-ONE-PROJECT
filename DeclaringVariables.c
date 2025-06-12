42254c61d597e147113798217311ca33b5bd7bb9
nourah30 <nestamwafongo522@gmail.com>
2025-06-12 17:12:48 +0200

    TOP1-2 COMMIT#2 ON BRANCH#1

--- DeclaringVariables.c
+++ DeclaringVariables.c
@@ -2,11 +2,11 @@
 #include "TaskOneProject.h"
 
  void declaringVariables(){
-     int age = 22;
+     int age = 26;
      float height = 5.9;
      double distance = 12345.6789;
      char Initial = 'A';
- printf("NEW AGE = %d\n" , age);
+ printf("updated AGE = %d\n" , age);
   printf("HEIGHT = %f\n" , height);
    printf("DISTANCE= %lf\n" , distance);
     printf("INITIAL = %C\n" , Initial);
