5b2d259ad742377e7bac7084cfd6d3218013260f
nourah30 <nestamwafongo522@gmail.com>
2025-06-12 17:02:05 +0200

    Initial

---
+++ DeclaringVariables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "TaskOneProject.h"
+
+ void declaringVariables(){
+     int age = 20;
+     float height = 5.9;
+     double distance = 12345.6789;
+     char Initial = 'A';
+ printf("AGE = %d\n" , age);
+  printf("HEIGHT = %f\n" , height);
+   printf("DISTANCE= %lf\n" , distance);
+    printf("INITIAL = %C\n" , Initial);
+
+ }
