88393b79de1b13bea89847aa836a4e8df5e08252
nourah30 <nestamwafongo522@gmail.com>
2025-06-17 18:27:24 +0200

    TOP1-4 COMMIT#1 ON BRANCH#4

--- DeclaringVariables.c
+++ DeclaringVariables.c
@@ -4,11 +4,11 @@
  void declaringVariables(){
      int age = 26;
      float height = 7.3;
-     double distance = 12345.0;
+     double distance = 12345.0156;
      char Initial = 'M';
  printf("updated AGE = %d\n" , age);
   printf("NEWEST HEIGHT = %f\n" , height);
-   printf("NEW DISTANCE= %lf\n" , distance);
+   printf("NEW. DISTANCE= %lf\n" , distance);
     printf("NEWEST INITIAL = %C\n" , Initial);
 
  }
